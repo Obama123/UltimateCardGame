5f71f074423199e53f89b8bf55b09d5f8015b7ef
066956210 <066956210@109R0305737HQ22.EDU.YRDSB.NET>
2015-10-07 11:56:44 -0400

    Yes We Can

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -4,18 +4,45 @@
  * and open the template in the editor.
  */
 package cardgame;
-
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  *
  * @author 068681097
  */
-public class CardGame {
+public class CardGame  extends JPanel {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   
+         public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+
+                    JFrame f = new JFrame("Java Game");
+
+                    f.setSize(1300, 800);
+                    f.setResizable(false);
+                    f.setVisible(true);
+                    f.setBackground(Color.GRAY);
+                    f.setContentPane(new CardGame());
+                    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-    
 }
+
+    
+
