3909bc11258c9e40db5ba4dd59b859bcbb2b2f02
066956210 <066956210@109R0305737HQ22.EDU.YRDSB.NET>
2015-10-14 11:55:45 -0400

    You Stupid

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -9,23 +9,49 @@ public class CardGame { //extends JPanel {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        int pointVal1=0;
+        int pointVal2=0;
+        int pointVal3=0;
+        int pointVal4=0;
+        int pointVal5=0;
         System.out.println("welcome to blackjack");
         System.out.println("how many people want to play (5 max)");
         int playerCount = keyboard.nextInt();
         int run = 0;
         while (run == 0){
-        if (playerCount <= 5 && playerCount >=1) { 
+        if (playerCount <= 5 && playerCount >=1)
+        { 
         System.out.println(playerCount+ " Players will be entered in this game");
         break;
                 }
-        else{
+        if (playerCount >5){
             System.out.println("too many players");
-            
-            
+            System.out.println ("Enter again: ");
+            playerCount = keyboard.nextInt();
+            if (playerCount <1){
+            System.out.println("too few players");
+            System.out.println ("Enter again: ");
+            playerCount = keyboard.nextInt();    
+            }
             }
         
         
         }
+        do{
+            System.out.println("P1's Cards:");
+            
+          if (pointVal1==21)  
+              System.out.println("P1 got Blackjack");
+          if (pointVal2==21)  
+              System.out.println("P2 got Blackjack");
+          if (pointVal3==21)  
+              System.out.println("P3 got Blackjack");
+          if (pointVal4==21)  
+              System.out.println("P4 got Blackjack");
+          if (pointVal5==21)  
+              System.out.println("P5 got Blackjack");
+        }while (pointVal1==21);
+        //win
     }
 }
 
